9a720f7e2274c4c5bde160128252fbe52206f775
Melih Ömer Kamar <melih.o.k@hotmail.com>
2025-04-17 16:47:43 +0300

    feat: DbContext configuration and GenericRepository service registry ok.

---
+++ src/Services/ContactService/ContactService.Infrastructure/Extensions/ServiceRegistry.cs
@@ -0,0 +1,20 @@
+﻿using ContactService.Application.Interfaces;
+using ContactService.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactService.Infrastructure.Extensions
+{
+    public static class ServiceRegistry
+    {
+        public static void AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseNpgsql(configuration.GetConnectionString("default"));
+            });
+            services.AddScoped(typeof(IGenericRepository<>), typeof(EfGenericRepository<>));
+        }
+    }
+}
