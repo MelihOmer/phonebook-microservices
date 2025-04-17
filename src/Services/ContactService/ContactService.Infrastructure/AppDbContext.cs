a990179439b1d7c7232005e3cc40893749c6558d
Melih Ömer Kamar <melih.o.k@hotmail.com>
2025-04-17 16:43:17 +0300

    feat: completed EfGenericRepository.

---
+++ src/Services/ContactService/ContactService.Infrastructure/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ContactService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactService.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+        public DbSet<Contact> Contacts{ get; set; }
+        public DbSet<ContactInformation> ContactInformation { get; set; }
+    }
+}
---
+++ src/Services/ContactService/ContactService.Infrastructure/Repository/EfGenericRepository.cs
@@ -0,0 +1,55 @@
+﻿using ContactService.Application.Interfaces;
+using ContactService.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ContactService.Infrastructure.Repository
+{
+    public class EfGenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly DbSet<T> _table;
+        public EfGenericRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+            _table = _appDbContext.Set<T>();
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            var result = await _table.AddAsync(entity);
+            return result.Entity;
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await GetByIdAsync(id);
+            entity.IsDeleted = true;
+            await UpdateAsync(entity);
+        }
+
+        public async Task<IList<T>> GetAllAsync()
+        {
+            var result = await _table.AsNoTracking().ToListAsync();
+            return result;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsyncByFilter(Expression<Func<T, bool>> predicate)
+        {
+            var result = await _table.AsNoTracking().Where(predicate).ToListAsync();
+            return result;
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            var result = await _table.Where(x => x.Id == id).FirstOrDefaultAsync();
+            return result;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _appDbContext.Entry(entity).State = EntityState.Modified;
+            return entity;
+        }
+    }
+}
