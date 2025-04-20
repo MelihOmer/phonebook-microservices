47414041c70d3bb14b87939d47fd46217680526a
Melih Ömer Kamar <melih.o.k@hotmail.com>
2025-04-20 03:39:31 +0300

    feat: ContactInformation service metots ok.

---
+++ src/Services/ContactService/ContactService.Application/Interfaces/Services/IContactInformationService.cs
@@ -0,0 +1,12 @@
+﻿using ContactService.Application.DTOs.ContactInformation;
+
+namespace ContactService.Application.Interfaces.Services
+{
+    public interface IContactInformationService
+    {
+        Task<ContactInfoResponseDto> CreateContactInformationAsync(ContactInfoCreateDto contactInfoCreateDto);
+        Task<ContactInfoResponseDto> GetContactInformationByIdAsync(Guid id);
+        Task<IEnumerable<ContactInfoResponseDto>> GetContactInformationsByContactIdAsync(Guid contactId);
+        Task RemoveContactInformationAsync(Guid id);
+    }
+}
---
+++ src/Services/ContactService/ContactService.Infrastructure/Services/ContactInformationService.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using ContactService.Application.DTOs.ContactInformation;
+using ContactService.Application.Interfaces.Repositories;
+using ContactService.Application.Interfaces.Services;
+using ContactService.Domain.Entities;
+using ContactService.Infrastructure.Persistence;
+using PhonebookMicroservices.Shared.Exceptions;
+
+namespace ContactService.Infrastructure.Services
+{
+    public class ContactInformationService : IContactInformationService
+    {
+        private readonly IContactInformationRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly AppDbContext _dbContext;
+
+        public ContactInformationService(AppDbContext dbContext, IMapper mapper, IContactInformationRepository repository)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _repository = repository;
+        }
+
+        public async Task<ContactInfoResponseDto> CreateContactInformationAsync(ContactInfoCreateDto contactInfoCreateDto)
+        {
+            var contactInfo = _mapper.Map<ContactInformation>(contactInfoCreateDto);
+            var result = await _repository.CreateContactInformationAsync(contactInfo);
+            await _dbContext.SaveChangesAsync();
+            var mappingResponse = _mapper.Map<ContactInfoResponseDto>(result);
+            return mappingResponse;
+        }
+
+        public async Task<ContactInfoResponseDto> GetContactInformationByIdAsync(Guid id)
+        {
+            var contactInfo = await _repository.GetContactInformationByIdAsync(id);
+            if (contactInfo == null)
+                throw new NotFoundException($"({id}) ID İletişim bilgisi bulunamadı.");
+            var mappingResponse = _mapper.Map<ContactInfoResponseDto>(contactInfo);
+            return mappingResponse;
+        }
+
+        public async Task<IEnumerable<ContactInfoResponseDto>> GetContactInformationsByContactIdAsync(Guid contactId)
+        {
+            var contactInfos = await _repository.GetContactInformationByExpressionAsync(x => x.ContanctId == contactId);
+            if (!contactInfos.Any())
+                throw new NotFoundException($"({contactId}) ID Kişiye ait iletşim bilgisi bulunamadı.");
+            var mappingResponse = _mapper.Map<IEnumerable<ContactInfoResponseDto>>(contactInfos);
+            return mappingResponse;
+        }
+
+        public async Task RemoveContactInformationAsync(Guid id)
+        {
+            var contactInfo = await _repository.GetContactInformationByIdAsync(id);
+            if (contactInfo == null)
+                throw new NotFoundException($"({id}) ID İletişim bilgisi bulunamadı.");
+            await _repository.RemoveContactInformationAsync(id);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
