76f9409239b498c085df0c1ede4681957f702d45
Melih Ömer Kamar <melih.o.k@hotmail.com>
2025-04-22 00:50:53 +0300

    feat: ReportService domain completed.

---
+++ src/Services/ReportService/ReportService.Domain/Entities/Report.cs
@@ -0,0 +1,12 @@
+﻿using ReportService.Domain.Common;
+using ReportService.Domain.Enums;
+
+namespace ReportService.Domain.Entities
+{
+    public class Report : BaseEntity
+    {
+        public DateTime RequestedAt { get; set; } = DateTime.UtcNow;
+        public ReportStatus ReportStatus { get; set; } = ReportStatus.Preparing;
+        public IEnumerable<ReportDetail> ReportDetails { get; set; }
+    }
+}
