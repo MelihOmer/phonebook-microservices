c549ee77c349cceffbc99a16bf3a9c4c1c80947c
Melih Ömer Kamar <melih.o.k@hotmail.com>
2025-04-21 11:39:52 +0300

    test: Contact GetByIdAsync test ok.

--- ContactService.Tests/Application/Features/Contacts/ContactServiceTests.cs
+++ ContactService.Tests/Application/Features/Contacts/ContactServiceTests.cs
@@ -5,6 +5,7 @@ using ContactService.Domain.Entities;
 using ContactService.Infrastructure.Services;
 using FluentAssertions;
 using Moq;
+using PhonebookMicroservices.Shared.Exceptions;
 
 namespace ContactService.Tests.Application.Features.Contacts
 {
@@ -51,6 +52,47 @@ namespace ContactService.Tests.Application.Features.Contacts
 
             _mockRepository.Verify(r => r.GetAllContactsAsync(), Times.Once());
             _mockMapper.Verify(m => m.Map<IEnumerable<ContactResponseDto>>(contacts), Times.Once());
+        }
+        [Fact]
+        public async Task GetContactByIdAsync_WhenContactExists_ShouldReturnMapperDto()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            var contact = new Contact
+            {
+                Id = id,
+                Firstname = "Melih Ömer",
+                Lastname = "KAMAR",
+                Company = "Company x"
+            };
+            var expectedDto = new ContactResponseDto(contact.Id,contact.Firstname,contact.Lastname, contact.Company);
+            _mockRepository.Setup(repo => repo.GetContactByIdAsync(id))
+                .ReturnsAsync(contact);
+            _mockMapper.Setup(map => map.Map<ContactResponseDto>(contact))
+                .Returns(expectedDto);
+            //Act
+            var result = await _contactService.GetContactByIdAsync(id);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ContactResponseDto>();
+            result.Id.Should().Be(id);
+            result.Firstname.Should().Be("Melih Ömer");
+            result.Company.Should().Be("Company x");
+        }
+        [Fact]
+        public async Task GetContactByIdAsync_WhenContactDoesNotExists_ShouldThrowNotFoundException()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            _mockRepository.Setup(r => r.GetContactByIdAsync(id))
+                .ReturnsAsync((Contact)null!);
+            //Act
+            var act = async () => await _contactService.GetContactByIdAsync(id);
+
+            //Assert
+            await act.Should()
+                .ThrowAsync<NotFoundException>();
         }
     }
 }
