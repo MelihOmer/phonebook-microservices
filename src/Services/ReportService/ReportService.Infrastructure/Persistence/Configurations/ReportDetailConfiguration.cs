7193d4e51a6ee1bc0f02eaf88b3df728083b82e9
Melih Ömer Kamar <melih.o.k@hotmail.com>
2025-04-22 02:17:44 +0300

    feat: EFReportDetail configuration added.

---
+++ src/Services/ReportService/ReportService.Infrastructure/Persistence/Configurations/ReportDetailConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReportService.Domain.Entities;
+
+namespace ReportService.Infrastructure.Persistence.Configurations
+{
+    public class ReportDetailConfiguration : IEntityTypeConfiguration<ReportDetail>
+    {
+        public void Configure(EntityTypeBuilder<ReportDetail> builder)
+        {
+            builder.ToTable("report_details");
+
+            builder.Property(x => x.Id)
+                .HasColumnName("id");
+            builder.Property(x => x.ReportId)
+               .HasColumnName("report_id");
+            builder.Property(x => x.PersonCount)
+               .HasColumnName("person_count");
+            builder.Property(x => x.PhoneCount)
+               .HasColumnName("phone_count");
+            builder.Property(x => x.Location)
+               .HasColumnName("location");
+
+            builder.HasOne(x => x.Report)
+                .WithMany(y => y.ReportDetails)
+                .HasForeignKey(x => x.ReportId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
