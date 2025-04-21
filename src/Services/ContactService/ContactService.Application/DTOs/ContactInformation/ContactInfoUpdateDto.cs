292bfea5d54a3193addec6d0248d3be1c48946b3
Melih Ömer Kamar <melih.o.k@hotmail.com>
2025-04-21 21:04:26 +0300

    test: ContactInfoIUpdateDtoValidator & ContactInfoCreateDtoValidator test ok.

--- src/Services/ContactService/ContactService.Application/DTOs/ContactInformation/ContactInfoUpdateDto.cs
+++ src/Services/ContactService/ContactService.Application/DTOs/ContactInformation/ContactInfoUpdateDto.cs
@@ -4,6 +4,13 @@ namespace ContactService.Application.DTOs.ContactInformation
 {
     public record ContactInfoUpdateDto
     {
+        public ContactInfoUpdateDto(Guid id, ContactInfoType type, string infoContent)
+        {
+            Id = id;
+            Type = type;
+            InfoContent = infoContent;
+        }
+
         public Guid Id { get; set; }
         public ContactInfoType Type { get; set; }
         public string InfoContent { get; set; }
---
+++ src/Services/ContactService/ContactService.Tests/Application/Features/ContactInformation/Validators/ContactInfoCreateDtoValidatorTests.cs
@@ -0,0 +1,86 @@
+﻿using ContactService.Application.DTOs.ContactInformation;
+using ContactService.Application.Validators.ContactInfoValidators;
+using ContactService.Domain.Enums;
+using FluentValidation.TestHelper;
+
+namespace ContactService.Tests.Application.Features.ContactInformation.Validators
+{
+    public class ContactInfoCreateDtoValidatorTests
+    {
+        private readonly ContactInfoCreateDtoValidator _validator=new();
+
+        [Fact]
+        public void ContactId_Should_Have_Error_When_Empty()
+        {
+            //Arrange
+            var dto = new ContactInfoCreateDto(Guid.Empty, ContactInfoType.Email, "test@mail.com");
+            //Act
+            var result = _validator.TestValidate(dto);
+            //Assert
+            result.ShouldHaveValidationErrorFor(x => x.ContactId);
+        }
+        [Fact]
+        public void Type_Should_Have_Error_When_Invalid_Enum()
+        {
+            //Arrange
+            var dto = new ContactInfoCreateDto(Guid.NewGuid(), (ContactInfoType)10, "test");
+            //Act
+            var result = _validator.TestValidate(dto);
+            //Assert
+            result.ShouldHaveValidationErrorFor(x => x.Type);
+        }
+        [Fact]
+        public void InfoContent_Should_Have_Error_When_Empty()
+        {
+            //Arrange
+            var dto = new ContactInfoCreateDto(Guid.NewGuid(),ContactInfoType.Location, "");
+            //Act
+            var result = _validator.TestValidate(dto);
+            //Assert
+            result.ShouldHaveValidationErrorFor(x => x.InfoContent);
+        }
+        [Fact]
+        public void InfoContent_Phone_Should_Have_Error_When_Invalid_Format()
+        {
+            //Arrange
+            var dto = new ContactInfoCreateDto(Guid.NewGuid(), ContactInfoType.Phone, "asd1234asd");
+            //Act
+            var result = _validator.TestValidate(dto);
+            //Assert
+            result.ShouldHaveValidationErrorFor(x => x.InfoContent);
+        }
+        [Fact]
+        public void InfoContent_Email_Should_Have_Error_When_Invalid_Format()
+        {
+            //Arrange
+            var dto = new ContactInfoCreateDto(Guid.NewGuid(), ContactInfoType.Email, "melih.com");
+            //Act
+            var result = _validator.TestValidate(dto);
+            //Assert
+            result.ShouldHaveValidationErrorFor(x => x.InfoContent);
+        }
+        [Fact]
+        public void InfoContent_Location_Should_Have_Error_When_TooShort()
+        {
+            //Arrange
+            var dto = new ContactInfoCreateDto(Guid.NewGuid(), ContactInfoType.Location, "s");
+            //Act
+            var result = _validator.TestValidate(dto);
+            //Assert
+            result.ShouldHaveValidationErrorFor(x => x.InfoContent);
+        }
+        [Theory]
+        [InlineData(ContactInfoType.Phone,"0505 090 07 04")]
+        [InlineData(ContactInfoType.Email,"melihomerkamar0@gmail.com")]
+        [InlineData(ContactInfoType.Location,"İstanbul")]
+        public void All_Valid_Inputs_Should_Not_Have_Errors(ContactInfoType type,string infoContent)
+        {
+            //Arrange
+            var dto = new ContactInfoCreateDto(Guid.NewGuid(), type, infoContent);
+            //Act
+            var result = _validator.TestValidate(dto);
+            //Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
